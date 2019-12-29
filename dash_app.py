58866536e1862ffec25747f29144d2f934944ed3
kaustavbhattacharjee <kaustavbhatt94@gmail.com>
2019-12-28 21:58:17 -0500

    feature: added graphs using px.parallelcoordinates

--- dash_app.py
+++ dash_app.py
@@ -128,32 +128,50 @@ def update_graph(year):
     title = "Model re-ranking vs Original ranking for the selected year"
     df1 = pd.read_csv('finance datasets/all.csv')
     df = df1.loc[df1['1-qid'] == int(year)]
-    # fig = make_subplots(rows=5, cols=2,
-    #             specs=[[{"type": "domain"}, {"type": "domain"}],
-    #                 [{"type": "domain"}, {"type": "domain"}],
-    #                 [{"type": "domain"}, {"type": "domain"}],
-    #                 [{"type": "domain"}, {"type": "domain"}],
-    #                 [{"type": "domain"}, {"type": "domain"}]]
-    # )
-    # 1
-    #  left
-    # fig.add_trace(
-    fig1 = go.Parcoords(
-        dimensions = list([
-            dict(range = [1,50],
-                constraintrange = [15,35],
-                label = "Ground Truth", values = df['2-realRank']),
+
+    df2 = df[['2-realRank', 'map_AdaRank_rerank']]
+    fig1 = px.parallel_coordinates(df2)
+    '''
+    df2 = df[['2-realRank', 'map_LambdaMART_rerank']]
+    fig2 = px.parallel_coordinates(df2)
     
-            dict(range = [1,50],
-                label = 'Ada Rerank', values = df['map_AdaRank_rerank'])
+    df2 = df[['2-realRank', 'map_LambdaRank_rerank']]
+    fig3 = px.parallel_coordinates(df2)
+    
+    df2 = df[['2-realRank', 'map_ListNet_rerank']]
+    fig4 = px.parallel_coordinates(df2)
+    
+    df2 = df[['2-realRank', 'map_MART_rerank']]
+    fig5 = px.parallel_coordinates(df2)
+
+    
+    df2 = df[['2-realRank', 'map_RankBoost_rerank']]
+    fig6 = px.parallel_coordinates(df2)
+
+    df2 = df[['2-realRank', 'map_RankNet_rerank']]
+    fig7 = px.parallel_coordinates(df2)
+
+    df2 = df[['2-realRank', 'map_coordinate_ascent_rerank']]
+    fig8 = px.parallel_coordinates(df2)
+
+    df2 = df[['2-realRank', 'map_linear_regression_rerank']]
+    fig9 = px.parallel_coordinates(df2)
+
+    df2 = df[['2-realRank', 'map_random_forest_rerank']]
+    fig10 = px.parallel_coordinates(df2)
+
+    '''
+    fig2 = go.Parcoords(
+        dimensions=list([
+            dict(range=[1, 50],
+                 constraintrange=[15, 35],
+                 label="Ground Truth", values=df['2-realRank']),
+
+            dict(range=[1, 50],
+                 label='LambdaMART Rerank', values=df['map_LambdaMART_rerank'])
         ])
     )
-    #     row=1,
-    #     col=1
-    # ),
-    # 2
-    # Right
-    # fig.add_trace(
+    '''
     fig2 = go.Parcoords(
         dimensions = list([
             dict(range = [1,50],
@@ -298,7 +316,8 @@ def update_graph(year):
 
     # fig.update_layout(autosize=False, height=1500)
         # fig.show()
-    return [fig1, fig2, fig3, fig4, fig5, fig6, fig7, fig8, fig9, fig10]
+    '''
+    return [fig1, fig2,fig2, fig2, fig2, fig2, fig2, fig2, fig2, fig2]
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
